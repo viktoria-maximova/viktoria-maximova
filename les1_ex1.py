1c638038afd1a806d6462f2f944cea68cad6d53a
Viktoria Maximova <vavivl@mail.ru>
2022-01-23 22:13:14 +0300

    Lesson1 added

---
+++ les1_ex1.py
@@ -0,0 +1,27 @@
+# Поработайте с переменными, создайте несколько, выведите на экран.
+
+name = 'ivan'
+surname = 'ivanov'
+age = 20
+
+print(name)
+print(surname)
+print(age)
+
+# #Запросите у пользователя некоторые числа и строки и сохраните в переменные, затем выведите на экран.
+
+year_of_birth = int (input('Ваш год рождения? '))
+print (year_of_birth)
+#print (type (year_of_birth))
+
+pet = int (input ('Сколько у Вас домашних животных? '))
+print (pet)
+#print (type (year_of_birth))
+
+colour = input ('Ваш любимый цвет? ')
+print (colour)
+#print (type (colour))
+
+city = input ('В каком городе Вы проживаете? ')
+print (city)
+#print (type (city))
